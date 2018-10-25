93787ffbec04daca888e2d3f2603ca6ce86d0e32
Non-Note <chanon.cherklin@gmail.com>
2018-10-25 14:22:39 +0700

    GetHobbyService

--- Client/client-test.py
+++ Client/client-test.py
@@ -1,5 +1,14 @@
 from suds.client import Client
 
-client = Client('https://flasktest-01.herokuapp.com/?wsdl', cache=None)
+#client = Client('https://flasktest-01.herokuapp.com/?wsdl', cache=None)
+client = Client('http://localhost:8000/?wsdl', cache=None)
 
-print(client.service.say_hello(u'J?r?me', 5))
+#print(client.service.say_hello(u'J?r?me', 5));
+d = client.service.get_hobby();
+#print(type(d));
+#file = open("data.xml", "w");
+#for i in range(len(d)):
+	#file.write(d[i]+'');
+#;
+#file.close();
+print(d);
