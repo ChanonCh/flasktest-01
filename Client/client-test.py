e1ee244e3555fbf748fe5e273044e1edce4d2c3d
Non-Note <chanon.cherklin@gmail.com>
2018-10-25 15:39:20 +0700

    Parcel Send,status Undone

--- Client/client-test.py
+++ Client/client-test.py
@@ -4,7 +4,9 @@ from suds.client import Client
 client = Client('http://localhost:8000/?wsdl', cache=None)
 
 #print(client.service.say_hello(u'J?r?me', 5));
-d = client.service.get_hobby();
+#d = client.service.get_hobby();
+d = client.service.send_parcel_data(u'Non',u'Palm',u'Mainroad',20);
+d = client.service.get_parcel_status();
 #print(type(d));
 #file = open("data.xml", "w");
 #for i in range(len(d)):
