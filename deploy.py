8fcd5a0c6f46d06c1d01d454f3fa3f65ca2db033
Non <chanon.cherklin@gmail.com>
2018-11-01 07:59:12 +0700

    All service with elementtree

--- deploy.py
+++ deploy.py
@@ -1,8 +1,8 @@
 from spyne import Application, rpc, ServiceBase, Iterable, Integer, Unicode
-
 from spyne.protocol.soap import Soap11
 from spyne.server.wsgi import WsgiApplication
 
+import xml.etree.ElementTree as et
 
 class HelloWorldService(ServiceBase):
     @rpc(Unicode, Integer, _returns=Iterable(Unicode))
@@ -27,29 +27,81 @@ class GetHobbyService(ServiceBase):
 		
 class ParcelService(ServiceBase):
 	@rpc(Unicode, Unicode, Unicode , Integer, Integer, _returns=Iterable(Unicode))
-	def send_parcel_data(ctx, sname, rname, location, weight, status=0):
+	def send_parcel_data(ctx, sname, rname, location, weight, status="0"):
 		#file = open("static\data.txt", "w");
-		file = open("static\parceldata.xml","w");
-		file.write('<parceldata>');
-		file.write('<sender>'+ str(sname) +'</sender>');
-		file.write('<receiver>' + str(rname) + '</receiver>');
-		file.write('<location>' + str(location)+'</location>');
-		file.write('<weight>'+str(weight)+'</weight>');
-		file.write('<status>'+str(status)+'</status>');
-		file.write('</parceldata>');
-		file.close();
+		file_path = "static\parceldata.xml";
+		tree = et.parse(file_path);
+		root = tree.getroot();
+		allid = root.findall("id");
+		lastid = allid[len(allid)-1].text;
+		new_parcel = et.SubElement(root,"id");
+		new_sender = et.SubElement(new_parcel,"sender");
+		new_receiver = et.SubElement(new_parcel,"receiver");
+		new_location = et.SubElement(new_parcel,"location");
+		new_weight = et.SubElement(new_parcel,"weight");
+		new_status = et.SubElement(new_parcel,"status");
+		
+		
+		if (lastid == None) :
+		    lastid = 0;
+		    new_parcel.text = str(int(lastid)+1);
+		else:
+		    new_parcel.text = str(int(lastid)+1);
+		new_sender.text = str(sname);
+		new_receiver.text = str(rname);
+		new_location.text = str(location);
+		new_weight.text = str(weight);
+		new_status.text = "On Shiping";
+		tree.write(file_path);
+		
+		#file = open("static\parceldata.xml","w");
+		#file.write('<parceldata>');
+		#file.write('<sender>'+ str(sname) +'</sender>');
+		#file.write('<receiver>' + str(rname) + '</receiver>');
+		#file.write('<location>' + str(location)+'</location>');
+		#file.write('<weight>'+str(weight)+'</weight>');
+		#file.write('<status>'+str(status)+'</status>');
+		#file.write('</parceldata>');
+		#file.close();
 		
 		return [u'Save done!!'];
 	
 class GetParcelStatusService(ServiceBase):
-	@rpc(_returns=Iterable(Unicode))
-	def get_parcel_status(f):
-		file = open("static\parceldata.xml", 'r');
-		return file;
+	@rpc(Integer,_returns=Iterable(Unicode))
+	def get_parcel_status(ctx,idn):
+		file_path = "static\parceldata.xml";
+		tree = et.parse(file_path);
+		root = tree.getroot();
+		elm = root.findall("id");
+		stat = "";
+		for i in elm:
+		    if(str(idn) == i.text):
+			stat = i.find("status").text;
+		
+		return [stat];
+	    
+class ChangeParcelStatusService(ServiceBase):
+	@rpc(Unicode,Integer,_returns=Iterable(Unicode))
+	def change_parcel_status(ctx,status,idn):
+		file_path = "static\parceldata.xml";
+		tree = et.parse(file_path);
+		root = tree.getroot();
+		elm = root.findall("id");
+		stat = status;
+		#print(str(type(status)) + " IDN is here");
+		for i in elm:
+		    #print(i.find("status").text);
+		    if(str(idn) == i.text):
+			i.find("status").text = str(status);
+			#print("If in" + str(i.find("status").text));
+		    #else:
+			#print("Else in");
+		tree.write(file_path);
+		return [u'Change done!'];
 	
 
 
-application = Application([HelloWorldService, GetHobbyService ,ParcelService ,GetParcelStatusService], 'spyne.examples.hello.soap',
+application = Application([HelloWorldService, GetHobbyService ,ParcelService ,GetParcelStatusService,ChangeParcelStatusService], 'spyne.examples.hello.soap',
                           in_protocol=Soap11(validator='lxml'),
                           out_protocol=Soap11())
 
